bc8b72ec3fcc4f732a11d1f60e042007cb440732
grggls <gregory.damiani@gmail.com>
2018-02-03 21:21:50 +0000

    clean up the lint warning about line-too-long. too lazy to try and disable

--- doctests.py
+++ doctests.py
@@ -51,9 +51,9 @@ def run_validate_tests():
     >>> foo = CronTab('*/5 * * * * ls -la')
     >>> foo.validate_minute()
     '0 5 10 15 20 25 30 35 40 45 50 55'
-    >>> foo = CronTab('* * * * * ls -la')
+    >>> foo = CronTab('*/10 * * * * ls -la')
     >>> foo.validate_minute()
-    '0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59'
+    '0 10 20 30 40 50'
     >>> foo = CronTab('1,2,3 * * * * ls -la')
     >>> foo.validate_minute()
     '1 2 3'
